4db7a469fb3a97b41c536f13ddef387b5d35ad25
Laurie Bax <47383333+lauriebax@users.noreply.github.com>
2025-06-20 09:44:52 +0000

    remove official support for python < 3.10

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import glob
 import os
 import platform
 import re
-from importlib.metadata import PackageNotFoundError, version
 from setuptools import find_packages, setup
 
 from packaging.version import parse as parse_version
@@ -28,112 +27,6 @@ try:
 except ModuleNotFoundError:
     cmd_class = {}
     print('Skip building ext ops due to the absence of torch.')
-
-print(f'{EXT_TYPE=}')
-
-
-def choose_requirement(primary, secondary):
-    """If some version of primary requirement installed, return primary, else
-    return secondary."""
-    try:
-        name = re.split(r'[!<>=]', primary)[0]
-        version(name)
-    except PackageNotFoundError:
-        return secondary
-
-    return str(primary)
-
-
-def parse_requirements(fname='requirements/runtime.txt', with_version=True):
-    """Parse the package dependencies listed in a requirements file but strips
-    specific versioning information.
-
-    Args:
-        fname (str): path to requirements file
-        with_version (bool, default=False): if True include version specs
-
-    Returns:
-        List[str]: list of requirements items
-
-    CommandLine:
-        python -c "import setup; print(setup.parse_requirements())"
-    """
-    import sys
-    from os.path import exists
-    require_fpath = fname
-
-    def parse_line(line):
-        """Parse information from a line in a requirements text file."""
-        if line.startswith('-r '):
-            # Allow specifying requirements in other files
-            target = line.split(' ')[1]
-            for info in parse_require_file(target):
-                yield info
-        else:
-            info = {'line': line}
-            if line.startswith('-e '):
-                info['package'] = line.split('#egg=')[1]
-            else:
-                # Remove versioning from the package
-                pat = '(' + '|'.join(['>=', '==', '>']) + ')'
-                parts = re.split(pat, line, maxsplit=1)
-                parts = [p.strip() for p in parts]
-
-                info['package'] = parts[0]
-                if len(parts) > 1:
-                    op, rest = parts[1:]
-                    if ';' in rest:
-                        # Handle platform specific dependencies
-                        # http://setuptools.readthedocs.io/en/latest/setuptools.html#declaring-platform-specific-dependencies
-                        version, platform_deps = map(str.strip,
-                                                     rest.split(';'))
-                        info['platform_deps'] = platform_deps
-                    else:
-                        version = rest  # NOQA
-                    info['version'] = (op, version)
-            yield info
-
-    def parse_require_file(fpath):
-        with open(fpath) as f:
-            for line in f.readlines():
-                line = line.strip()
-                if line and not line.startswith('#'):
-                    yield from parse_line(line)
-
-    def gen_packages_items():
-        if exists(require_fpath):
-            for info in parse_require_file(require_fpath):
-                parts = [info['package']]
-                if with_version and 'version' in info:
-                    parts.extend(info['version'])
-                if not sys.version.startswith('3.4'):
-                    # apparently package_deps are broken in 3.4
-                    platform_deps = info.get('platform_deps')
-                    if platform_deps is not None:
-                        parts.append(';' + platform_deps)
-                item = ''.join(parts)
-                yield item
-
-    packages = list(gen_packages_items())
-    return packages
-
-
-install_requires = parse_requirements()
-
-try:
-    # OpenCV installed via conda.
-    import cv2  # NOQA: F401
-    major, minor, *rest = cv2.__version__.split('.')
-    if int(major) < 3:
-        raise RuntimeError(
-            f'OpenCV >=3 is required but {cv2.__version__} is installed')
-except ImportError:
-    # If first not installed install second package
-    CHOOSE_INSTALL_REQUIRES = [('opencv-python-headless>=3',
-                                'opencv-python>=3')]
-    for main, secondary in CHOOSE_INSTALL_REQUIRES:
-        install_requires.append(choose_requirement(main, secondary))
-
 
 def get_extensions():
     extensions = []
@@ -179,7 +72,6 @@ def get_extensions():
             pytorch=True)
         extensions.append(ext_ops)
     elif EXT_TYPE == 'pytorch':
-        print('YES')
         ext_name = 'mmcv._ext'
         from torch.utils.cpp_extension import CppExtension, CUDAExtension
 
@@ -259,7 +151,6 @@ def get_extensions():
             libraries += ['torch_dipu']
         elif is_rocm_pytorch or torch.cuda.is_available() or os.getenv(
                 'FORCE_CUDA', '0') == '1':
-            print('b')
             if is_rocm_pytorch:
                 define_macros += [('MMCV_WITH_HIP', None)]
             define_macros += [('MMCV_WITH_CUDA', None)]
@@ -276,7 +167,6 @@ def get_extensions():
         elif (hasattr(torch, 'is_mlu_available') and
                 torch.is_mlu_available()) or \
                 os.getenv('FORCE_MLU', '0') == '1':
-            print('c')
             from torch_mlu.utils.cpp_extension import MLUExtension
 
             def get_mluops_version(file_path):
@@ -376,7 +266,6 @@ def get_extensions():
         elif (hasattr(torch.backends, 'mps')
               and torch.backends.mps.is_available()) or os.getenv(
                   'FORCE_MPS', '0') == '1':
-            print('d')
             # objc compiler support
             from distutils.unixccompiler import UnixCCompiler
             if '.mm' not in UnixCCompiler.src_extensions:
@@ -401,7 +290,6 @@ def get_extensions():
             include_dirs.append(os.path.abspath('./mmcv/ops/csrc/common'))
             include_dirs.append(os.path.abspath('./mmcv/ops/csrc/common/mps'))
         elif (os.getenv('FORCE_NPU', '0') == '1'):
-            print('e')
             print(f'Compiling {ext_name} only with CPU and NPU')
             try:
                 import importlib
@@ -436,7 +324,6 @@ def get_extensions():
             include_dirs.append(os.path.abspath('./mmcv/ops/csrc/common'))
             include_dirs.append(os.path.abspath('./mmcv/ops/csrc/common/npu'))
         elif hasattr(torch, 'musa') or os.getenv('FORCE_MUSA', '0') == '1':
-            print('f')
             from torch_musa.testing import get_musa_arch
             from torch_musa.utils.musa_extension import MUSAExtension
             define_macros += [('MMCV_WITH_MUSA', None),
@@ -451,7 +338,6 @@ def get_extensions():
             include_dirs.append(os.path.abspath('./mmcv/ops/csrc/common/musa'))
             extension = MUSAExtension
         else:
-            print('g')
             print(f'Compiling {ext_name} only with CPU')
             op_files = glob.glob('./mmcv/ops/csrc/pytorch/*.cpp') + \
                 glob.glob('./mmcv/ops/csrc/pytorch/cpu/*.cpp')
@@ -488,7 +374,6 @@ def get_extensions():
 setup(
     name='mmcv' if os.getenv('MMCV_WITH_OPS', '1') == '1' else 'mmcv-lite',
     packages=find_packages(),
-    python_requires='>=3.8',
     ext_modules=get_extensions(),
     cmdclass=cmd_class,
     zip_safe=False)
