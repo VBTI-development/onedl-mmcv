1950c536535fe80e02ed4026aeb360e1546a231d
Laurie Bax <47383333+lauriebax@users.noreply.github.com>
2026-01-09 11:12:43 +0000

    remove wrong files

--- docker/build/wheelhouse/build-jetpacks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-docker build -f Dockerfile.jetpack60 -t mmcv-builder:jp60-torch280 .
-docker run --rm -e MAX_JOBS=8 -v "$(pwd)/wheelhouse/dist/jp60":/out mmcv-builder:jp60-torch280
-
-docker build -f Dockerfile.jetpack61 -t mmcv-builder:jp61-torch280 .
-docker run --rm -e MAX_JOBS=8 -v "$(pwd)/wheelhouse/dist/jp61":/out mmcv-builder:jp61-torch280
