523ba634cd449aa01792fff38911f0c02805a11b
woldier <woldier@foxmail.com>
2026-05-11 16:52:59 +0800

    Fallback to PyTorch implementation for bf16 MultiScaleDeformableAttention (#44)

--- mmcv/ops/multi_scale_deform_attn.py
+++ mmcv/ops/multi_scale_deform_attn.py
@@ -367,11 +367,20 @@ class MultiScaleDeformableAttention(BaseModule):
         if ((IS_CUDA_AVAILABLE and value.is_cuda)
                 or (IS_MLU_AVAILABLE and value.is_mlu)
                 or (IS_MUSA_AVAILABLE and value.is_musa)
-                or (IS_NPU_AVAILABLE and value.device.type == 'npu')):
+                or (IS_NPU_AVAILABLE and value.device.type == 'npu'))\
+                and value.dtype != torch.bfloat16:
             output = MultiScaleDeformableAttnFunction.apply(
                 value, spatial_shapes, level_start_index, sampling_locations,
                 attention_weights, self.im2col_step)
         else:
+            if ((IS_CUDA_AVAILABLE and value.is_cuda)
+                or (IS_MLU_AVAILABLE and value.is_mlu)
+                or (IS_MUSA_AVAILABLE and value.is_musa)
+                or (IS_NPU_AVAILABLE and value.device.type == 'npu'))\
+                and value.dtype == torch.bfloat16:
+                warnings.warn(
+                    'MultiScaleDeformableAttnFunction does not support torch.bfloat16. '
+                    'Falling back to the PyTorch implementation.')
             output = multi_scale_deformable_attn_pytorch(
                 value, spatial_shapes, sampling_locations, attention_weights)
 
